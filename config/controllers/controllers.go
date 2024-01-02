274a203afce6e607dbb51a226465a7943fe9546f
eron97 <eronsantosbetine@gmail.com>
2024-01-02 18:52:33 -0300

    finish

--- config/controllers/controllers.go
+++ config/controllers/controllers.go
@@ -1,4 +1,4 @@
-package controller
+package controllers
 
 import (
 	"github.com/eron97/LoginAuthenticator.git/config/models"
--- config/routes/routes.go
+++ config/routes/routes.go
@@ -1,9 +1,25 @@
 package routes
 
 import (
+	"github.com/eron97/LoginAuthenticator.git/config/controllers"
+	"github.com/eron97/LoginAuthenticator.git/config/services"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupTaskRoutes(router *gin.Engine) {
-	router.GET("/AllUsers")
+// SetupTaskRoutes configura as rotas para o UserController
+func SetupTaskRoutes(router *gin.Engine, userService *services.UserService) {
+	userController := controllers.NewUserController(userService)
+
+	router.GET("/AllUsers", func(c *gin.Context) {
+		// Chama o manipulador GetUsersHandler do UserController
+		users, err := userController.GetUsersHandler()
+		if err != nil {
+			// Handle error (log, return a specific error, etc.)
+			c.JSON(500, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		// Retorna os usuários como resposta
+		c.JSON(200, users)
+	})
 }
